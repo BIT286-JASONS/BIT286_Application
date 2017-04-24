863afd856b14bf13820d5dd483d94aa4cb94a40a
lucas <kirby317@msn.com>
2017-04-24 14:22:30 -0700

    added account view methods

--- BIT286_Application/Controllers/AccountController.cs
+++ BIT286_Application/Controllers/AccountController.cs
@@ -9,12 +9,29 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using BIT286_Application.Models;
+using BIT286_Application.ViewModels;
 
 namespace BIT286_Application.Controllers
 {
     
     public class AccountController : Controller
     {
-        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult Scores()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePassword(NewPassword newpassword)
+        {
+            return View();
+        }
     }
 }
